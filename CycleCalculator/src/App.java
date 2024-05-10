6dd9d958c859880e257773ce979f6390fd200a5c
CBrandsDev <cbrandao2001@gmail.com>
2024-05-10 08:38:52 -0300

    prints

--- CycleCalculator/src/App.java
+++ CycleCalculator/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) {
-        
+        Prints print = new Prints();
+
+        print.clean();
+        print.main();
+        print.clean();
+        print.fertile();
+        print.clean();
+        print.redRiver();
     }
 }
--- CycleCalculator/src/Prints.java
+++ CycleCalculator/src/Prints.java
@@ -18,10 +18,23 @@ public class Prints {
     public void main() {
         System.out.println("  Cycle Calculator  ");
         System.out.println("                    ");
+        System.out.println("   Welcome Camila   ");
         System.out.println("                    ");
-        System.out.println("                    ");
-        System.out.println("                    ");
-        System.out.println("                    ");
-        System.out.println("                    ");
+        System.out.println("   Type the date    ");
+        System.out.println("      of your       ");
+        System.out.println("   last menstrual   ");
+        System.out.println("      period:       ");
+    }
+
+    public void fertile() {
+        System.out.println("   Caution on this  ");
+        System.out.println("        days        ");
+        System.out.println("   Fertile Period:  ");
+        
+    }
+
+    public void redRiver() {
+        System.out.println("   Next menstrual   ");
+        System.out.println("       period:      ");
     }
 }
